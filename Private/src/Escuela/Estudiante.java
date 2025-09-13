7ff095e4650c5940baf1160cf5f8315610f11abb
Daniel Otero <doteroe@unicartagena.edu.co>
2025-09-12 23:15:18 -0500

    Agregamos la clase coche para los ejercicios del taller 4

---
+++ Private/src/Escuela/Estudiante.java
@@ -0,0 +1,29 @@
+package Escuela;
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private double notaPromedio;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public double getNotaPromedio() {
+        return notaPromedio;
+    }
+    public void setNotaPromedio(double notaPromedio) {
+        this.notaPromedio = notaPromedio;
+    }
+
+    
+
+}
---
+++ Private/src/Escuela/Prueba.java
@@ -0,0 +1,17 @@
+package Escuela;
+public class Prueba {
+    public static void main(String[] args) {
+        
+    
+
+    Estudiante e1 = new Estudiante();
+
+    e1.setNombre("Daniel");
+    e1.setEdad(20);
+    e1.setNotaPromedio(4.5);
+
+    e1.nombre = "daniel";
+
+    }
+
+}
