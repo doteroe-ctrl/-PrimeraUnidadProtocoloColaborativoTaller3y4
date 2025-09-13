28767fad74cfd84a40e2f42f6fceab7dbb1dbf75
Daniel Otero <doteroe@unicartagena.edu.co>
2025-09-13 00:23:13 -0500

    Movemos la clase coche al paquete vehiculos y modifico prueba en escuela para los ejercicios del taller 4

--- Private/src/Escuela/Prueba.java
+++ Private/src/Escuela/Prueba.java
@@ -6,11 +6,11 @@ public class Prueba {
 
     Estudiante e1 = new Estudiante();
 
-    e1.setNombre("Daniel");
+    e1.setNombre("Daniel"); //accedemos atraves de setters que son public
     e1.setEdad(20);
     e1.setNotaPromedio(4.5);
 
-    e1.nombre = "daniel";
+    // e1.nombre = "daniel"; //no se puede acceder porque el atributo es private
 
     }
 
---
+++ Private/src/Vehiculos/Prueba.java
@@ -0,0 +1,10 @@
+package Vehiculos;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Coche c1 = new Coche();
+
+        c1.acelerar(-4);
+    }
+
+}
