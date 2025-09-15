21b776eae0028e4368b4fb46a0f37b9121a965a7
pablocubides <pcubidesp@unicartagena.edu.co>
2025-09-15 17:47:50 -0500

    taller4 1,2,3

--- Static/src/Ejercicio2/prueba.java
+++ Static/src/Ejercicio2/prueba.java
@@ -4,7 +4,7 @@ public class prueba {
     public static void main(String[] args) {
         
         int A = 20;
-        int B = 3;
+        int B = 2;
         System.out.println("Suma:"+ Matematicas.Suma(A, B));
         System.out.println("Resta:"+ Matematicas.Resta(A, B));
         System.out.println("Multiplicacion:"+ Matematicas.Multiplicacion(A, B));
